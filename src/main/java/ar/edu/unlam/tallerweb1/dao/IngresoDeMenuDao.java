ae4cb9223bacd2be4049b907221f7a357e8c9c7d
Gallo-Claudio <galloclaudio69@gmail.com>
2019-06-23 15:06:23 -0300

    limpieza 2 - se eliminaros clases obsoletas

--- src/main/java/ar/edu/unlam/tallerweb1/dao/IngresoDeMenuDao.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.unlam.tallerweb1.dao;
-
-import ar.edu.unlam.tallerweb1.modelo.Reserva;
-
-public interface IngresoDeMenuDao {
-	
-	void guardarMenuSeleccionado (Reserva menuSeleccionado);
-}
